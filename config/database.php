33f98b81184b5619af640878a24512de8fb67a29
Skywodd <skywodd@gmail.com>
2014-06-05 08:35:35 +0200

    Add configuration files

---
+++ config/database.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of LICPRO-PHP-ProjetWeb.
+ *
+ * LICPRO-PHP-ProjetWeb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LICPRO-PHP-ProjetWeb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ----- Database configuration ----- */
+
+/**
+ * Database host name.
+ */
+define('DATABASE_HOST', 'localhost');
+
+/**
+ * Database name to use.
+ */
+define('DATABASE_NAME', 'DriveCommunal');
+
+/**
+ * Database user name (must have all autorizations on the database).
+ */
+define('DATABASE_USERNAME', 'root');
+
+/**
+ * Database password.
+ */
+define('DATABASE_PASSWORD', '');
